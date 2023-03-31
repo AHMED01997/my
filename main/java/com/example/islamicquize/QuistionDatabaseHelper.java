6da00f174df11b8ebc95d98aee6dcc262e9f10a2
AHMED01997 <126155775+AHMED01997@users.noreply.github.com>
2023-03-31 21:58:35 +0200

    Update QuistionDatabaseHelper.java

--- main/java/com/example/islamicquize/QuistionDatabaseHelper.java
+++ main/java/com/example/islamicquize/QuistionDatabaseHelper.java
@@ -10,64 +10,38 @@ import java.util.ArrayList;
 
 public class QuistionDatabaseHelper extends SQLiteOpenHelper {
 
-  public static final String DATABASE_NAME = "quistions.db";
+    public static final String DATABASE_NAME = "quistions.db";
 
 
-   public  QuistionDatabaseHelper(Context context) {
-       super(context, DATABASE_NAME, null, 1);
-   }
+    public  QuistionDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
 
-   @Override
-   public void onCreate(SQLiteDatabase db) {
-       db.execSQL("CREATE TABLE quistions (id INTEGER PRIMARY KEY AUTOINCREMENT, quistion TEXT, op1 TEXT, op2 TEXT, op3 TEXT, op4 TEXT, answer TEXT)");
-   }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_QUISTIONS_TABLE = "CREATE TABLE " + "quistions" +
+                "(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "quistion TEXT, " +
+                "op1 TEXT, " +
+                "op2 TEXT, " +
+                "op3 TEXT, " +
+                "op4 TEXT, " +
+                "answer TEXT" +
+                ")";
+        String CREATE_USER_QUESTIONS_TABLE = "CREATE TABLE " + "user_questions" +
+                "(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "question_text TEXT, " +
+                "answer_text TEXT" +
+                ")";
+        db.execSQL(CREATE_QUISTIONS_TABLE);
+        db.execSQL(CREATE_USER_QUESTIONS_TABLE);
+    }
 
-   @Override
-   public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-       db.execSQL("DROP TABLE IF EXISTS quistions;");
-       onCreate(db);
-   }
-   public boolean insertData(String quistion,String op1 ,String op2 ,String op3 ,String op4 , String answer){
-       SQLiteDatabase db =this.getWritableDatabase();
-       ContentValues contentValues = new ContentValues();
-       contentValues.put("quistion",quistion);
-       contentValues.put("op1",op1);
-       contentValues.put("op2",op2);
-       contentValues.put("op3",op3);
-       contentValues.put("op4",op4);
-       contentValues.put("answer",answer);
-       long result =db.insert("quistions",null,contentValues);
-       if(result==-1)
-           return false;
-       else
-           return true;
-
-
-   }
-
-public ArrayList getAllRecord(){
-    ArrayList arrayList =new ArrayList() ;
-    SQLiteDatabase db =this.getReadableDatabase();
-   Cursor res =db.rawQuery("select * from quistions",null);
-   res.moveToFirst();
-   while (res.isAfterLast()==false){
-
-       String t1= res.getString(0);
-       String t2= res.getString(1);
-       String t3= res.getString(2);
-       String t4= res.getString(3);
-       String t5= res.getString(4);
-       String t6= res.getString(5);
-       arrayList.add(t1+t2+t3+t4+t5+t6);
-       res.moveToNext();
-
-
-   }
-
-     return arrayList;
-
-
-}
-
-
-}
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + "quistions");
+        db.execSQL("DROP TABLE IF EXISTS " + "user_questions");
+        onCreate(db);
+    }}
